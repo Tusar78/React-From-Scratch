a5aaf03ee69d02896bbb148846cc30e5e8013431
Tusar <tusarahmed141@gmail.com>
2022-12-31 12:41:11 +0600

    Load country using fetch and also async awat

---
+++ Module-44/JS/App.js
@@ -0,0 +1,19 @@
+const loadCountry = () => {
+  fetch("https://restcountries.com/v3.1/all")
+    .then((res) => res.json())
+    .then((data) => displayCountry(data));
+};
+
+const asyncCountry = async () => {
+  const response = await fetch('https://restcountries.com/v3.1/all');
+  const data = await response.json();
+  console.log(data);
+}
+
+const displayCountry = countries => {
+  console.log(countries);
+}
+
+// loadCountry();
+
+asyncCountry()
