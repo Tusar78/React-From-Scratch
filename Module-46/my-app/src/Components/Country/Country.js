1db35ffe8e69a8323aaa1d6e180f20766d5fcf8a
Tusar <tusarahmed141@gmail.com>
2023-01-04 01:03:43 +0600

    Advanced destructuring

--- Module-46/my-app/src/Components/Country/Country.js
+++ Module-46/my-app/src/Components/Country/Country.js
@@ -1,11 +1,11 @@
 import React from 'react';
 
 const Country = ({country}) => {
-  const {name, area, flags} = country;
+  const {name: {common}, area, flags} = country;
   return (
     <div className='country'>
-      <img className='country__flag' src={flags.png} alt={name} />
-      <h2 className='country__title'>{name.common}</h2>
+      <img className='country__flag' src={flags.png} alt={common} />
+      <h2 className='country__title'>{common}</h2>
       <span className='country__text'>{area}</span>
     </div>
   );
