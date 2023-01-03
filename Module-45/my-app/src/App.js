e50c45f315dc58490aa440f8804147601533bbac
Tusar <tusarahmed141@gmail.com>
2023-01-03 12:23:59 +0600

    Going to explore useEffects

--- Module-45/my-app/src/App.js
+++ Module-45/my-app/src/App.js
@@ -1,10 +1,9 @@
 import React from "react";
-import IncreaseDecrease from "./Components/IncreaseDecrease/IncreaseDecrease";
 
 const App = () => { 
   return (
     <div className="container">
-      <IncreaseDecrease />
+      
     </div>
   );
 };
