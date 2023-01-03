33e1b9275c0678b0ee9873e1cbd1e586cccde875
Tusar <tusarahmed141@gmail.com>
2023-01-04 00:28:25 +0600

    Install necessary tools called tailwind

--- Module-46/my-app/src/App.js
+++ Module-46/my-app/src/App.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import './App.css';
+import Countries from './Components/Countries/Countries';
 
 const App = () => {
   return (
-    <div>
-      
-    </div>
+    <>
+      <Countries />
+    </>
   );
 };
 
