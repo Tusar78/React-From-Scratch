8b2764529127375b7b1a9b3ffbea33106b171b2a
Tusar <tusarahmed141@gmail.com>
2023-01-03 12:05:31 +0600

    Decrease functionality also done

--- Module-45/my-app/src/Components/IncreaseDecrease/IncreaseDecrease.js
+++ Module-45/my-app/src/Components/IncreaseDecrease/IncreaseDecrease.js
@@ -9,6 +9,7 @@ const IncreaseDecrease = () => {
       <div>
         <h2>Increased: {increase}</h2>
         <button onClick={() => setIncrease(increase + 1)}>Increase + </button>
+        <button onClick={() => setIncrease(increase - 1)}>Decrease - </button>
       </div>
     </div>
   );
