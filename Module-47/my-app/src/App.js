32f74e0230f4002c217b6f93031d58b6fef9c7ca
Tusar <tusarahmed141@gmail.com>
2023-01-04 14:16:31 +0600

    going to experement event listener

--- Module-47/my-app/src/App.js
+++ Module-47/my-app/src/App.js
@@ -3,19 +3,9 @@ import React from 'react';
 const App = () => {
   return (
     <div>
-      <TestComponent name="Tusar" />
+      
     </div>
   );
 };
-
-const TestComponent = (properties) => {
-  const {name} = properties;
-  console.log(name);
-  return (
-    <div>
-      <h2>Hello World</h2>
-    </div>
-  )
-}
 
 export default App;
